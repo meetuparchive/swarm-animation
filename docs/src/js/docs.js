c72d4f0e94ab6aaf0374210b1338d20bc3d5e03a
ivy <ivy@meetup.com>
2017-03-15 11:13:08 -0400

    trying to separate the js for the docs

---
+++ docs/src/js/docs.js
@@ -0,0 +1,42 @@
+import $ from 'jquery';
+import swarmAnimation from 'js/lib.js';
+
+$(function() {
+	// Highlightjs - fancy tables parsed from markdown
+	/* $(document).ready(function() { */
+		// $('pre code').each(function(i, block) {
+			// hljs.highlightBlock(block);
+		// });
+		// $('.doc_block-desc > table').addClass('stripedTable');
+	// });
+
+	// Disable links without real href
+	$('a').on('click', function(e) {
+		if (this.getAttribute('href') == '#' || $(this).hasClass('noFollow')) {
+			e.preventDefault();
+		}
+		return;
+	});
+
+	// collapsible example
+	const $c = $('#cContainer'),
+		CLASS_COL_CLOSE = 'collapsible--close';
+	if ($c.length > 0) {
+		$('#cTrigger').on('click', function() {
+			if ($c.hasClass(CLASS_COL_CLOSE) ) {
+				$c.removeClass(CLASS_COL_CLOSE);
+			} else {
+				$c.addClass(CLASS_COL_CLOSE);
+			}
+		});
+	}
+
+	// animation fade example
+	// TODO move to own partial
+	$('#fadeOutLink').on('click', function() {
+		debugger
+		swarmAnimation.hide($('.animateEl'));
+	});
+
+});
+
